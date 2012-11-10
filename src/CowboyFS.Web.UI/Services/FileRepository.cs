b493074cee779ae63e869a35fc5ece505460c74e
Padgett Rowell <padgett@padgett.com.au>
2012-11-10 17:17:03 +0800

    formatting changes

--- src/CowboyFS.Web.UI/Services/FileRepository.cs
+++ src/CowboyFS.Web.UI/Services/FileRepository.cs
@@ -25,30 +25,29 @@ namespace CowboyFS.Web.UI.Services
         {
             NameValueCollection configuredLibries = (NameValueCollection)ConfigurationManager.GetSection("libraries");
 
-            for (int i = 0; i < configuredLibries.Count;i++ )
+            for (int i = 0; i < configuredLibries.Count; i++)
             {
                 yield return new Library(i + 1, configuredLibries.GetValues(i)[0], configuredLibries.Keys[i]);
             }
         }
 
         public IEnumerable<FileResult> FetchResultsForPath(Library library, string relativePath)
-         {
-             string fullPath = string.Format("{0}{1}",library.FullPathName,relativePath);
+        {
+            string fullPath = string.Format("{0}{1}", library.FullPathName, relativePath);
 
-             var folders = FileSystemEnumerator.GetDirectories(fullPath, "*.*");
-             foreach (string r in folders)
-             {
+            var folders = FileSystemEnumerator.GetDirectories(fullPath, "*.*");
+            foreach (string r in folders)
+            {
 
-                 yield return new FileResult(library.MakePathRelativeToLibrary(r), FileType.Folder);
-             }
+                yield return new FileResult(library.MakePathRelativeToLibrary(r), FileType.Folder);
+            }
 
-             var files = FileSystemEnumerator.GetFiles(fullPath, "*.*");
-             foreach (string r in files)
-             {
-                 yield return new FileResult(library.MakePathRelativeToLibrary(r), FileType.UnknownFile);
-             }
+            var files = FileSystemEnumerator.GetFiles(fullPath, "*.*");
+            foreach (string r in files)
+            {
+                yield return new FileResult(library.MakePathRelativeToLibrary(r), FileType.UnknownFile);
+            }
 
-         }
-
+        }
     }
 }
