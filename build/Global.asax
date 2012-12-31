84ec68a3d26150103c55545e05fdd65ef303b446
Padgett Rowell <padgett@padgett.com.au>
2012-12-31 11:15:20 +0800

    Added WiX installer
    Shout out to @mark_kranz for contributing this

--- build/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CowboyFS.Web.UI.MvcApplication" Language="C#" %>
---
+++ src/CowboyFS.Installer/build.bat
@@ -0,0 +1,17 @@
+REM Setting variables...
+REM Name of the folder where the 'publish' from msbuild target will be performed
+set publishFolder=..\Setup\publish\
+REM Remove complete publish folder in order to be sure that evrything will be newly compiled
+rmdir /S /Q %publishFolder%
+REM Rebuild entire solution
+msbuild /p:Configuration=Release /t:ReBuild ..\CowboyFS.Web.UI.sln
+REM Publish your web site
+msbuild /t:ResolveReferences;_CopyWebApplication /p:Configuration=Release;OutDir=%publishFolder%\bin\;WebProjectOutputDir=%publishFolder% ..\CowboyFS.Web.UI\CowboyFS.Web.UI.csproj
+REM Delete debug and setup web.configs to prevent 'heat' from harveting it
+del %publishFolder%Web.Debug.config
+del %publishFolder%Web.Release.config
+REM Harvest all content of published result
+heat dir %publishFolder% -dr MYWEBWEBSITE -ke -srd -cg MyWebWebComponents -var var.publishDir -gg -out WebSiteContent.wxs
+REM At last create an installer
+candle -ext WixIISExtension -ext WixUtilExtension -ext WiXNetFxExtension -dpublishDir=%publishFolder% -dMyWebResourceDir=. IisConfiguration.wxs Product.wxs WebSiteContent.wxs CowboyFSUI.wxs
+light -ext WixUIExtension -ext WixIISExtension -ext WixUtilExtension -ext WiXNetFxExtension -out bin\Release\CowboyFS.msi Product.wixobj WebSiteContent.wixobj CowboyFSUI.wixobj IisConfiguration.wixobj
